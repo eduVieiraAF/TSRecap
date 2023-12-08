0efc5c7650d59a3a89907484cb6ff993ebb6886d
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-08 14:45:58 -0300

    refactoring + types

---
+++ 002/index.ts
@@ -0,0 +1,16 @@
+const request: string = "onion rings"
+const count: number = 5
+const tip: boolean = false
+
+
+function order(request: string, count: number, tip: boolean) {
+    if (!tip) {
+        console.log("Tip not included")
+        console.log(`Your total is $${count * 14.99}`)
+    } else {
+        console.log("Tip included")
+        console.log(`Your total is $${(count * 14.99) * 1.15 }`)
+    }
+}
+
+order(request, count, tip)
