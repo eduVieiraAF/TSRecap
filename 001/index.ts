8507cb779f96b779f531be5048e07cb4190df857
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-08 14:36:35 -0300

    first commit

---
+++ 001/index.ts
@@ -0,0 +1,19 @@
+// Typescript
+// npm install -g typescript
+// tsc --init
+
+// Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + alt + n
+
+
+const _name: string = "Eduardo"
+
+if (_name.includes("rd")) console.log("okay")
+else console.log("not okay")
+
+const request = (num: number) => {
+    console.log(num)
+}
+
+request(15)
