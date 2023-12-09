80d95fdfa522aabf193b6ac0c07eaed494324056
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-08 21:39:58 -0300

    more

--- 008/stringLiteralTypes.ts
+++ 008/stringLiteralTypes.ts
@@ -1,3 +1,7 @@
 let mouseEvent: 'click' | 'dblclick' | 'mousedown' | 'mouseup' = 'click';
 
 mouseEvent = 'dblclick';
+
+let color: 'red' | 'green' | 'blue' = 'red';    
+
+console.log(color, mouseEvent);
