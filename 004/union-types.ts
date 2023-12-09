63c0a535faac2c5eca89fe71ebf91ba24b25000e
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-08 21:15:04 -0300

    union types

---
+++ 004/union-types.ts
@@ -0,0 +1,10 @@
+/*
+    * Union Types - A union type is a combination of multiple types
+*/
+
+let value: string | number = "hello";
+
+value = 1;
+value = "world";
+
+// made 'value' interchangeable between string and number
